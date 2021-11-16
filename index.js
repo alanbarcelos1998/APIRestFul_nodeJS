21a1dd6447040e222154f5fdd8de624034024650
alanbarcelos1998 <alanbasiliobarcelos@hotmail.com>
2021-11-16 08:44:13 -0300

    finalizado codigo com a api de pessoas

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // config inicial
+require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
@@ -26,8 +27,8 @@ app.get('/', (req,res) => {
 })
 
 // entregar uma porta
-const DB_USER = 'Alan'
-const DB_PASSWORD = encodeURIComponent('YdnwZZQxj3nwrs4C')
+const DB_USER = process.env.DB_USER
+const DB_PASSWORD = encodeURIComponent(process.env.DB_PASSWORD)
 
 mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@apicluster.hi9js.mongodb.net/bancodaapi?retryWrites=true&w=majority`
 ).then(() => {
--- routes/personRoutes.js
+++ routes/personRoutes.js
@@ -3,7 +3,6 @@ const router = express.Router()
 
 const Person = require('../models/Person')
 
-/
 // Create - criação de dados
 router.post('/', async (req, res) => {
 
@@ -14,6 +13,7 @@ router.post('/', async (req, res) => {
 
     if (!name) {
         res.status(422).json({error: 'O nome é obrigatório!'})
+        return
     }
 
     const person = {
@@ -34,5 +34,81 @@ router.post('/', async (req, res) => {
 })
 
 // Read - leitura de dados
+router.get('/', async (req,res) => {
+    try{
+        const people = await Person.find()
+
+        res.status(200).json(people)
+
+    } catch(error){
+        res.status(500).json({error: error})
+    }
+})
+
+router.get('/:id', async (req,res) => {
+    // Extrair o dado da requisição pela url = req.params
+
+    const id = req.params.id
+
+    try{
+        const person = await Person.findOne({_id: id})
+
+        if(!person) {
+            res.status(422).json({message: 'O usuário não foi encontrado!'})
+            return
+        }
+
+        res.status(200).json(person)
+    } catch(error){
+        res.status(500).json({ error: error})
+    }
+})
+
+// Update - atualização de dados (PUT, PATCH)
+router.patch('/:id', async (req,res)=>{
+    const id = req.params.id
+
+    const { name, salary, approved } = req.body
+
+    const person = {
+        name,
+        salary,
+        approved
+    }
+
+    try{
+        const updatePerson = await Person.updateOne({_id: id}, person)
+
+        if (updatePerson.matchedCount === 0){
+            res.status(422).json({message: 'O usuário não foi encontrado!'})
+            return
+        }
+
+        res.status(200).json(person)
+    } catch(error){
+        res.status(500).json({ error: error})
+    }
+})
+
+// Delete - deletar dados
+router.delete('/:id', async (req, res) => {
+    const id = req.params.id
+
+    const person = await Person.findOne({ _id: id})
+
+    if (!person) {
+        res.status(422).json({message: 'O usuário não foi encontrado!'})
+        return
+    }
+
+    try{
+
+        await Person.deleteOne({_id: id})
+
+        res.status(200).json({message: 'Usuário removido com sucesso'})
+    }catch(error){
+        res.status(500).json({ error: error})
+    }
+})
 
 module.exports = router
